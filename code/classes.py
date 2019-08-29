f0ddd5aedc97d77c9519a12df495424939c14c93
annayqho <annayqho@gmail.com>
2019-08-28 19:39:29 -0700

    code for diff classes of objects

---
+++ code/classes.py
@@ -0,0 +1,9 @@
+""" Events / parameter space for different class of transients """
+
+def snia():
+    # TBD
+
+
+def core_collapse():
+    # TBD
+
--- code/make_figure.py
+++ code/make_figure.py
@@ -3,12 +3,12 @@
 import matplotlib.pyplot as plt
 plt.rc("font", family="serif")
 plt.rc("text", usetex=True)
-from classes import *
+# from classes import *
 
 fig, ax = plt.subplots(1,1, figsize=(8,8))
 
 ax.set_xlabel("Time Above Half-Max (days)", fontsize=16)
-ax.set_ylabel("Peak Bolometric Magnitude", fontsize=16)
+ax.set_ylabel("Peak Luminosity ($M_v$)", fontsize=16)
 ax.set_xlim(0.1,100)
 ax.set_xscale('log')
 ax.set_ylim(-24, -6)
@@ -22,7 +22,7 @@ y_f = lambda y_i: 1E7 * 10**((y_i-71.2)/(-2.5))
 ymin, ymax = ax.get_ylim()
 ax2.set_ylim((y_f(ymin), y_f(ymax)))
 ax2.plot([],[])
-ax2.set_ylabel("Peak Bolometric Luminosity (erg/s)", 
+ax2.set_ylabel("Peak Luminosity (erg/s)", 
         fontsize=16, rotation=270, verticalalignment='bottom')
 ax2.tick_params(axis='both', labelsize=14)
 ax2.set_yscale('log')
